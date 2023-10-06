b93e7d6dd112c00f557dccca06b3c89a2328cf4e
Othamae <vero.gconesa@gmail.com>
2023-10-06 14:57:53 +0200

    feat: :truck: Rename track to music

--- src/store/playerStore.ts
+++ src/store/playerStore.ts
@@ -1,7 +1,7 @@
 import type { Playlist, Song } from '@/lib/data'
 import { create } from 'zustand'
 
-interface CurrentTrackType {
+interface CurrentMusicType {
     playlist: Playlist | null
     song: Song | null
     listOfSongs: Song[]
@@ -10,18 +10,18 @@ interface CurrentTrackType {
 export interface PlayerStoreStateType {
     isPlaying: boolean
     isLiked: boolean
-    currentTrack: CurrentTrackType
+    currentMusic: CurrentMusicType
     setIsPlaying: (isPlaying: boolean) => void
     setIsLiked: (isLiked: boolean) => void
-    setCurrentTrack: (currentTrack: CurrentTrackType) => void
+    setCurrentMusic: (currentMusic: CurrentMusicType) => void
 }
 
 
 export const usePlayerStore = create<PlayerStoreStateType>((set) => ({
     isPlaying: false,
     isLiked: false,
-    currentTrack: { playlist: null, song: null, listOfSongs: [] },
+    currentMusic: { playlist: null, song: null, listOfSongs: [] },
     setIsPlaying: (isPlaying: boolean) => set({ isPlaying }),
     setIsLiked: (isLiked: boolean) => set({ isLiked }),
-    setCurrentTrack: (currentTrack: CurrentTrackType) => set({ currentTrack })
+    setCurrentMusic: (currentMusic: CurrentMusicType) => set({ currentMusic })
 }))
