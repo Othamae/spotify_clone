4f4173ddd6a0c882c77c27bfd74825c1177274d0
Othamae <vero.gconesa@gmail.com>
2023-10-06 13:14:15 +0200

    feat: :sparkles: Added state to PlayButtonPlaylist

--- src/components/PlayButtonPlaylist.tsx
+++ src/components/PlayButtonPlaylist.tsx
@@ -1,17 +1,28 @@
+import PauseIcon from "@/icons/Pause.tsx"
 import PlayIcon from "@/icons/Play.tsx"
-//import PauseIcon from "@/icons/Pause.tsx"
+import { usePlayerStore } from "@/store/playerStore"
 
 interface PlayButtonPlaylistProps {
     className?: string
+    id: string
 
 }
 
-const PlayButtonPlaylist: React.FC<PlayButtonPlaylistProps> = ({ className }) => {
+const PlayButtonPlaylist: React.FC<PlayButtonPlaylistProps> = ({ className, id }) => {
+    const isPlaying = usePlayerStore(state => state.isPlaying)
+    const setIsPlaying = usePlayerStore(state => state.setIsPlaying)
+
+    const handleClick = () => {
+        setIsPlaying(!isPlaying)
+
+    }
     return (
         <button
+            onClick={handleClick}
             className={`text-black bg-green-500 rounded-full p-2 mr-4 shadow-lg shadow-gray-900 ${className}`}
         >
-            <PlayIcon />
+            {isPlaying ? <PauseIcon /> : <PlayIcon />}
+
         </button>
     )
 }
