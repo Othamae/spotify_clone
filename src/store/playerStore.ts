9c8074644f2b9cc05d474b44fcb8abbb96d1873d
Othamae <vero.gconesa@gmail.com>
2023-10-06 13:13:47 +0200

    feat: :label: Added types to playerStore

--- src/store/playerStore.ts
+++ src/store/playerStore.ts
@@ -1,15 +1,23 @@
 import type { Playlist, Song } from '@/lib/data'
 import { create } from 'zustand'
 
-interface currentTrackType {
-    playlist: Playlist
-    song: Song
+interface CurrentTrackType {
+    playlist: Playlist | null
+    song: Song | null
     listOfSongs: Song[]
 }
 
-export const usePlayerStore = create((set) => ({
+export interface PlayerStoreStateType {
+    isPlaying: boolean;
+    currentTrack: CurrentTrackType;
+    setIsPlaying: (isPlaying: boolean) => void;
+    setCurrentTrack: (currentTrack: CurrentTrackType) => void;
+}
+
+
+export const usePlayerStore = create<PlayerStoreStateType>((set) => ({
     isPlaying: false,
     currentTrack: { playlist: null, song: null, listOfSongs: [] },
     setIsPlaying: (isPlaying: boolean) => set({ isPlaying }),
-    setCurrentTrack: (currentTrack: currentTrackType) => set({ currentTrack })
+    setCurrentTrack: (currentTrack: CurrentTrackType) => set({ currentTrack })
 }))
