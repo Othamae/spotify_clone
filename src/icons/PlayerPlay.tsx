7f0c95bf508bf2da148b1b9a2d7165d37c3ccc27
Othamae <vero.gconesa@gmail.com>
2023-10-06 13:45:56 +0200

    refactor: :recycle: Added PlayerPause and PlayerPlay icons

---
+++ src/icons/PlayerPlay.tsx
@@ -0,0 +1,7 @@
+
+const PlayerPlay = () => (
+    <svg viewBox="0 0 24 24" className="h-7 w-7" fill="currentColor"
+    ><path fill="currentColor" d="M8 5.14v14l11-7-11-7z"></path></svg>
+)
+
+export default PlayerPlay
