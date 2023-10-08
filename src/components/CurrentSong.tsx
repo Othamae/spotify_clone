870ce171857758eee4bf0fb172b2a91f9248ad8f
Othamae <vero.gconesa@gmail.com>
2023-10-08 17:13:30 +0200

    refactor: :recycle: Added currentMusic from state in CurrentSong

--- src/components/CurrentSong.tsx
+++ src/components/CurrentSong.tsx
@@ -1,20 +1,16 @@
+import { usePlayerStore } from "@/store/playerStore"
 import HeartButton from "./HeartButton"
 
-interface CurrentSongProps {
-    image?: string
-    artist?: string
-    title?: string
-}
-
-const CurrentSong: React.FC<CurrentSongProps> = ({ image, artist, title }) => {
+const CurrentSong = () => {
+    const currentMusic = usePlayerStore(state => state.currentMusic)
     return (
         <div className="flex flex-row items-center gap-4 align-middle relative overflow-hidden">
             <picture className="aspect-square h-14 w-14 flex-none ">
-                <img src={image} alt={title} className="rounded-[4px] bg-cover" />
+                <img src={currentMusic?.song?.image} alt={currentMusic?.song?.title} className="rounded-[4px] bg-cover" />
             </picture>
             <div className="flex flex-col">
-                <h4 className="font-semibold">{title}</h4>
-                <span className="text-xs text-gray-400">{artist}</span>
+                <h4 className="font-semibold">{currentMusic?.song?.title}</h4>
+                <span className="text-xs text-gray-400">{currentMusic?.song?.artists.join(', ')}</span>
             </div>
             <div className="">
                 <HeartButton className="w-4 h-4" />
