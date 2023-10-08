19cb73fae1acac0f21e098e8112eaa4922f08b91
Othamae <vero.gconesa@gmail.com>
2023-10-08 15:29:01 +0200

    feat: :sparkles: Added Mute to volume

--- src/components/Volume.tsx
+++ src/components/Volume.tsx
@@ -1,23 +1,38 @@
 import { MaxVolume, MidVolume, MinVolume, MuteVolume } from '@/icons/VolumeIcons'
 import { usePlayerStore } from '@/store/playerStore'
 import { Slider } from './Slider'
+import { useRef } from 'react'
 
 const Volume = () => {
     const volume = usePlayerStore(state => state.volume)
     const setVolume = usePlayerStore(state => state.setVolume)
+    const previousVolume = useRef(volume)
+
+    const isVolumeSilence = volume === 0
+
+    const handleClick = () => {
+        isVolumeSilence ? setVolume(previousVolume.current) : (
+            previousVolume.current = volume,
+            setVolume(0)
+        )
+
+    }
 
     return (
         <div className='flex justify-center gap-x-2 text-white'>
+            <button onClick={handleClick} className="opacity-70 hover:opacity-100">
+                {volume <= 0 ? <MuteVolume /> :
+                    (volume < 0.3 ? <MinVolume /> : (
+                        volume < 0.7 ? <MidVolume /> : <MaxVolume />
+                    ))}
+            </button>
 
-            {volume <= 0 ? <MuteVolume /> :
-                (volume < 0.3 ? <MinVolume /> : (
-                    volume < 0.7 ? <MidVolume /> : <MaxVolume />
-                ))}
             <Slider
                 defaultValue={[100]}
                 max={100}
                 min={0}
                 className='w-[95px]'
+                value={[volume * 100]}
                 onValueChange={(value) => {
                     const [newVolume] = value
                     const volumeValue = newVolume / 100
