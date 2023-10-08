8a0c2155589520688bdb5cdf44951f6b34491386
Othamae <vero.gconesa@gmail.com>
2023-10-08 17:06:45 +0200

    feat: :sparkles: Created SongControl component

--- src/components/SongControl.tsx
+++ src/components/SongControl.tsx
@@ -1,46 +1,87 @@
-import { useEffect, useState } from "react"
-import { Slider } from "./Slider"
-interface SongControlProps {
-    audio: React.MutableRefObject<HTMLAudioElement | null>
-}
+import { PlayerBack, PlayerNext, PlayerPause, PlayerPlay } from '@/icons/PlayerIcons'
+import { usePlayerStore } from '@/store/playerStore'
+import { useEffect, useRef } from 'react'
+import SongDuration from './SongDuration'
 
-const SongControl: React.FC<SongControlProps> = ({ audio }) => {
-    const [currentTime, setCurrentTime] = useState(0)
+const SongControl = () => {
+    const isPlaying = usePlayerStore(state => state.isPlaying)
+    const setIsPlaying = usePlayerStore(state => state.setIsPlaying)
+    const currentMusic = usePlayerStore(state => state.currentMusic)
+    const setCurrentMusic = usePlayerStore(state => state.setCurrentMusic)
+    const volume = usePlayerStore(state => state.volume)
+    const setVolume = usePlayerStore(state => state.setVolume)
+    const audioRef = useRef<HTMLAudioElement | null>(null)
+
 
     useEffect(() => {
-        audio.current?.addEventListener('timeupdate', handleTimeUpdate)
-        return () => {
-            audio.current?.removeEventListener('timeupdate', handleTimeUpdate)
+        audioRef.current && (
+            isPlaying
+                ? audioRef.current.play()
+                : audioRef.current.pause()
+        )
+    }, [isPlaying])
+
+    useEffect(() => {
+        audioRef.current && (audioRef.current.volume = volume)
+    }, [volume])
+
+
+    useEffect(() => {
+        const { song, playlist } = currentMusic
+        if (song) {
+            const src = `/music/${playlist?.id}/0${song.id}.mp3`
+            audioRef.current && (
+                audioRef.current.src = src,
+                audioRef.current.volume = volume,
+                audioRef.current.play()
+            )
         }
-    }, [])
 
-    const handleTimeUpdate = () => {
-        setCurrentTime(audio.current?.currentTime || 0)
+    }, [currentMusic])
+
+
+
+    const handleClick = () => {
+        setIsPlaying(!isPlaying)
     }
 
-    const formatTime = (time: number) => {
-        if (time === 0) return '--:--'
-        const minutes = Math.floor(time / 60)
-        const seconds = Math.floor(time % 60)
-        return `${minutes}:${seconds.toString().padStart(2, '0')}`
-    }
-    const duration = audio.current?.duration || 0
 
+    const handlePreviousSong = () => {
+        const nextSong = currentMusic.song
+        if (nextSong) {
+            const index = currentMusic.listOfSongs.indexOf(nextSong)
+            const previousSong = currentMusic.listOfSongs[index - 1]
+            previousSong && setCurrentMusic({ ...currentMusic, song: previousSong })
+        }
+    }
+
+    const handleNextSong = () => {
+        const previousSong = currentMusic.song
+        if (previousSong) {
+            const index = currentMusic.listOfSongs.indexOf(previousSong)
+            const nextSong = currentMusic.listOfSongs[index + 1]
+            nextSong && setCurrentMusic({ ...currentMusic, song: nextSong })
+        }
+    }
     return (
-        <div className="flex flex-row gap-2 text-xs py-2">
-            <span className="opacity-60 w-12 text-right">{formatTime(currentTime)}</span>
-            <Slider
-                defaultValue={[0]}
-                max={audio?.current?.duration ?? 0}
-                min={0}
-                className='w-[500px]'
-                value={[currentTime]}
-                onValueChange={(value) => {
-                    audio.current && (audio.current.currentTime = value[0])
-                }}
-            />
-            <span className="opacity-60 w-12">{formatTime(duration)}</span>
+        <div className='flex justify-center flex-col items-center pt-1'>
+            <div className='flex flex-row gap-5 items-center'>
+                <button onClick={handlePreviousSong}
+                    className='text-white p-1 opacity-60 hover:opacity-100'>
+                    <PlayerBack />
+                </button>
+                <button onClick={handleClick}
+                    className='bg-white rounded-full p-1 text-black'>
+                    {isPlaying ? <PlayerPause /> : <PlayerPlay />}
+                </button>
+                <button onClick={handleNextSong}
+                    className='p-1 text-white opacity-60 hover:opacity-100'>
+                    <PlayerNext />
+                </button>
+            </div>
 
+            <SongDuration audio={audioRef} />
+            <audio ref={audioRef} />
         </div>
     )
 }
