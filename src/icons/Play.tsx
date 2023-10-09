24e49e808c55b8a3f9f7523981de0586b799952b
Othamae <vero.gconesa@gmail.com>
2023-10-09 12:57:42 +0200

    feat: :sparkles: Added className as props to PlayIcon

--- src/icons/Play.tsx
+++ src/icons/Play.tsx
@@ -1,6 +1,8 @@
-
-const Play = () => (
-    <svg viewBox="0 0 24 24" className="h-8 w-8" fill="currentColor"
+interface PlayProps {
+    className?: string
+}
+const Play: React.FC<PlayProps> = ({ className }) => (
+    <svg viewBox="0 0 24 24" className={className} fill="currentColor"
     ><path fill="currentColor" d="M8 5.14v14l11-7-11-7z"></path></svg>
 )
 
