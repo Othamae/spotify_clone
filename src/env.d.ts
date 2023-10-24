3ea09617e214815620b9ef5f183ad5e7c1b00ce1
Othamae <vero.gconesa@gmail.com>
2023-10-24 11:54:22 +0200

    docs: :memo: Added env needed to connect with Spotify API

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="astro/client" />
+/// <reference path="../.astro/types.d.ts" />
+
+interface ImportMetaEnv {
+    readonly CLIENT_ID: string
+    readonly CLIENT_SECRET: string
+    readonly REDIRECT_URI: string
+    readonly SCOPE: string
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
+
